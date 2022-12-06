9c02f7ba2dfd79a2412cd4757e4a6239e7358427
JohnBebee <1021222969@qq.com>
2022-12-06 10:05:12 +0800

    test commit

---
+++ johnbebee/src/main/java/TestJava.java
@@ -0,0 +1,11 @@
+/**
+ * @author JohnBebee
+ * @version 2022/12/6
+ * @Description:
+ * @Copyright (C)2022
+ */
+public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
