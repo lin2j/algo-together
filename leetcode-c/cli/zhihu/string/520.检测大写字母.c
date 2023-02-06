81cd82af346ac0026fb57a2f5d3fedbe1ade7d8d
lin2j <linjinjia047@163.com>
2023-02-06 16:56:31 +0800

    字符

---
+++ leetcode-c/cli/zhihu/string/520.检测大写字母.c
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=520 lang=c
+ *
+ * [520] 检测大写字母
+ *
+ * https://leetcode.cn/problems/detect-capital/description/
+ *
+ * algorithms
+ * Easy (57.01%)
+ * Likes:    228
+ * Dislikes: 0
+ * Total Accepted:    83.9K
+ * Total Submissions: 147.3K
+ * Testcase Example:  '"USA"'
+ *
+ * 我们定义，在以下情况时，单词的大写用法是正确的：
+ * 
+ * 
+ * 全部字母都是大写，比如 "USA" 。
+ * 单词中所有字母都不是大写，比如 "leetcode" 。
+ * 如果单词不只含有一个字母，只有首字母大写， 比如 "Google" 。
+ * 
+ * 
+ * 给你一个字符串 word 。如果大写用法正确，返回 true ；否则，返回 false 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：word = "USA"
+ * 输出：true
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：word = "FlaG"
+ * 输出：false
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= word.length <= 100
+ * word 由小写和大写英文字母组成
+ * 
+ * 
+ */
+#include <stdbool.h>
+
+// @lc code=start
+bool detectCapitalUse(char * word)
+{
+
+}
+// @lc code=end
+
+
