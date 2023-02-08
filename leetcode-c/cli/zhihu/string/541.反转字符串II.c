af95609a41bc3ee1482f8751fd8bfa46cf179c54
lin2j <linjinjia047@163.com>
2023-02-08 10:06:13 +0800

    字符串的反转

---
+++ leetcode-c/cli/zhihu/string/541.反转字符串II.c
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=541 lang=c
+ *
+ * [541] 反转字符串 II
+ *
+ * https://leetcode.cn/problems/reverse-string-ii/description/
+ *
+ * algorithms
+ * Easy (58.72%)
+ * Likes:    411
+ * Dislikes: 0
+ * Total Accepted:    170.6K
+ * Total Submissions: 290.7K
+ * Testcase Example:  '"abcdefg"\n2'
+ *
+ * 给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，
+ * 就反转这 2k 字符中的前 k 个字符。
+ * 
+ * 
+ * 如果剩余字符少于 k 个，则将剩余字符全部反转。
+ * 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：s = "abcdefg", k = 2
+ * 输出："bacdfeg"
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：s = "abcd", k = 2
+ * 输出："bacd"
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= s.length <= 10^4
+ * s 仅由小写英文组成
+ * 1 <= k <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+
+
+char * reverseStr(char *s, int k){
+
+}
+// @lc code=end
+
+
